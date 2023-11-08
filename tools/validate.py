f140c7a032551ac42b43f5accff6f4c755c26940
Dan Zeman <zeman@ufal.mff.cuni.cz>
2023-11-08 20:21:37 +0100

    Some tests are now done more generally and do not have to be repeated for events.

--- tools/validate.py
+++ tools/validate.py
@@ -1103,33 +1103,16 @@ def validate_events(sentence, node_dict, args):
         nid = node['variable']
         if 'event_reason' in node:
             # :ARG relations imply that it is an event but they are not required.
-            # On the other hand, :aspect and :modstr seem to be required according to the guidelines.
-            # Only one :aspect and one :modstr relation is expected.
+            # On the other hand, :aspect and :modal-strength seem to be required according to the guidelines.
             for rtype in [':aspect', ':modal-strength']:
                 relations = sorted([r for r in node['relations'] if r['dir'] == 'out' and r['relation'] == rtype], key=lambda x: x['line0'])
-                if len(relations) > 1:
-                    testid = 'non-unique-attribute'
-                    testmessage = "Repeated assertion of %s for node %s (other %s on line %d)." % (rtype, nid, rtype, relations[0]['line0'])
-                    warn(testmessage, testclass, testlevel, testid, lineno=relations[1]['line0'])
-                elif len(relations) < 1:
+                if len(relations) < 1:
                     testid = 'missing-attribute'
                     testmessage = "Missing attribute %s. Node %s is an event because %s." % (rtype, nid, node['event_reason'])
                     warn(testmessage, testclass, testlevel, testid, lineno=node['line0'])
                 elif relations[0]['type'] != 'atom':
                     testid = 'invalid-attribute'
                     testmessage = "Expected atomic value of attribute %s, found type=%s, value=%s." % (rtype, relations[0]['type'], relations[0]['value'])
-                    warn(testmessage, testclass, testlevel, testid, lineno=relations[0]['line0'])
-            for rtype in [':ARG0', ':ARG1', ':ARG2', ':ARG3', ':ARG4', ':ARG5', ':ARG6']:
-                irel = [r for r in node['relations'] if r['dir'] == 'in' and r['relation'] == rtype+'-of']
-                orel = [r for r in node['relations'] if r['dir'] == 'out' and r['relation'] == rtype]
-                relations = sorted(irel + orel, key=lambda x: x['line0'])
-                if len(relations) > 1:
-                    testid = 'non-unique-relation'
-                    testmessage = "Repeated relation %s for node %s (other %s on line %d)." % (rtype, nid, rtype, relations[0]['line0'])
-                    warn(testmessage, testclass, testlevel, testid, lineno=relations[1]['line0'])
-                elif len(relations) == 1 and relations[0]['type'] != 'node':
-                    testid = 'invalid-relation'
-                    testmessage = "Expected child node of relation %s, found type=%s, value=%s." % (relations[0]['relation'], relations[0]['type'], relations[0]['value'])
                     warn(testmessage, testclass, testlevel, testid, lineno=relations[0]['line0'])
 
 
