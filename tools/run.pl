67e123004f125731baa27eeb0fe2f70aad9ec3e8
Dan Zeman <zeman@ufal.mff.cuni.cz>
2026-07-10 09:01:53 +0300

    Both STDERR and STDOUT.

--- tools/run.pl
+++ tools/run.pl
@@ -14,5 +14,9 @@ if(scalar(@ARGV) == 0)
     die("Expected at least one argument");
 }
 my $commandline = join(' ', @ARGV);
+# If we are redirecting STDOUT to a file and we want the first line of that file
+# to show the commandline arguments, we must print this to STDOUT, too. But if
+# we still want to see it in the terminal, we must also print it to STDERR.
+print("Running: $commandline\n");
 print STDERR ("Running: $commandline\n");
 system($commandline);
