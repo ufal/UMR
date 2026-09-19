5252dbee7ff7fce428502444286e08508f84a592
Dan Zeman <zeman@ufal.mff.cuni.cz>
2026-09-19 11:01:44 +0200

    A script to map the folder structure of PDT-C files.

---
+++ tools/map_pdtc_files.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+# Zjistí, které soubory PDT-C patří do které složky.
+# Copyright © 2026 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# License: GNU GPL
+
+use utf8;
+use open ':utf8';
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+binmode(STDERR, ':utf8');
+use Cwd qw(abs_path);
+
+my $pdtcdir = '/net/work/people/zeman/hamledt/normalize/cs/data/source';
+my %directory;
+foreach my $part ('train', 'dev', 'test')
+{
+    my $path = "$pdtcdir/$part/tamw";
+    opendir my $dh, $path or die("Cannot read $path: $!");
+    for my $thing (readdir($dh))
+    {
+        if($thing =~ m/^(.+)\.t$/)
+        {
+            my $basename = $1;
+            my $target = readlink("$path/$thing");
+            if($target)
+            {
+                $directory{$basename} = abs_path($target);
+            }
+        }
+    }
+}
+my @basenames = sort(keys(%directory));
+foreach my $basename (@basenames)
+{
+    print("$basename\t--->\t$directory{$basename}\n");
+}
+printf("TOTAL %d files\n", scalar(@basenames));
